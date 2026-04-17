3103ff1d6e74ca6c655cb24963350a41de9d74ae
Al-aqua <mohammad@alaqua.dev>
2026-04-17 21:24:53 +0300

    Add transparent.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,12 +5,25 @@ return {
   { "vague-theme/vague.nvim" },
   { "sainnhe/everforest" },
   { "rebelot/kanagawa.nvim" },
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    opts = {
+      groups = {
+        "Normal",
+      },
+      extra_groups = {
+        "NormalFloat",
+        "NvimTreeNormal",
+      },
+    },
+  },
 
   -- Configure LazyVim to load the Theme you want
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "carbonfox",
+      colorscheme = "nightfox",
     },
   },
 }
